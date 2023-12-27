cc2959526caa1bd1ec3bc868fb74ac0f1ead54a7
Saurabh Asthana <xxx.@gmail.com>
2023-12-27 11:15:39 +0530

    fundamental part 1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,9 @@
 let js = 'amazing';
-if (js == 'amazing') alert('Javascript is FUN !');
+if (js == 'amazing') alert("Welcome to Javascript");
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
